e13e2c755943c456f542052e9dd7ba5c118bd73e
Andrew Hughes <andyashmore1@gmailcom>
2025-01-08 11:24:00 -0600

    add method to multply number

---
+++ src/main/java/com/promineotech/TestDemo.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.promineotech;
+
+import java.util.Random;
+
+/**
+ * 
+ */
+public class TestDemo {
+
+	public int addPositive(int a, int b) {
+		if (a <= 0 || b <= 0) {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+		return a + b;
+	}
+	
+	//multiplies two integers if both are positive
+	public int multiplyIfPositive(int a, int b) {
+		if (a <= 0 || b <= 0) {
+			throw new IllegalArgumentException("Both parameters must be positive");
+		}
+		return a * b;
+	}
+	
+	public int randomNumberSquared() {
+		int randomNum = getRandomInt();
+		return randomNum * randomNum;
+	}
+	
+	public int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	}
+	
+}
---
+++ src/test/java/com/promineotech/TestDemoJUnitTest.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package com.promineotech;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+/**
+ * 
+ */
+class TestDemoJUnitTest {
+
+	
+	private TestDemo testDemo;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		testDemo = new TestDemo();
+	}
+
+	@ParameterizedTest
+	@MethodSource("com.promineotech.TestDemoJUnitTest#argumentsForAddPositive")
+	void assertThatTwoPositiveNumbersAreAddedCorrectly(int a, int b, int expected, boolean expectException) {
+		if(!expectException) {
+			assertThat(testDemo.addPositive(a, b)).isEqualTo(expected);
+		}
+		else {
+			assertThatThrownBy(() -> 
+			testDemo.addPositive(a, b)).isInstanceOf(IllegalArgumentException.class);
+		}
+	}
+	
+	public static Stream<Arguments> argumentsForAddPositive() {
+		return Stream.of(
+				arguments(2, 4, 6, false),  // 2 + 4 = 6, no exception expected
+	            arguments(0, 5, 0, true),  // 0 + 5, exception expected
+	            arguments(-3, 7, 0, true), // -3 + 7, exception expected
+	            arguments(10, 20, 30, false), // 10 + 20 = 30, no exception
+	            arguments(-1, -2, 0, true) // Both negative, exception expected
+				);
+	}
+	
+	@Test
+	void assertThatPairsOfPositiveNumbersAreAddedCorrectly(int a, int b, int expected, boolean expectException) {
+		TestDemo testDemo = new TestDemo();
+
+        // Test cases for positive numbers
+        assertThat(testDemo.addPositive(4, 5)).isEqualTo(9);
+        assertThat(testDemo.addPositive(40, 50)).isEqualTo(90);
+        assertThat(testDemo.addPositive(1, 1)).isEqualTo(2);
+
+	}
+	
+	@Test
+	void assertThatPairsOfPositiveNumbersAreMultipliedCorrectly(int a, int b, int expected, boolean expectException) {
+		TestDemo testDemo = new TestDemo();
+		
+		// Test cases for positive numbers
+		assertThat(testDemo.multiplyIfPositive(4, 5)).isEqualTo(20);
+		assertThat(testDemo.multiplyIfPositive(40, 50)).isEqualTo(2000);
+		assertThat(testDemo.multiplyIfPositive(1, 1)).isEqualTo(2);
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
